9fb77f18a39cbfd60c13a1dd3882a3fd32094913
Unknown <harshil57@yahoo.com>
2018-03-14 16:38:33 -0400

    Added running page
    The running page layout is complete and will be connected to firebase when it is restored

---
+++ app/src/main/java/com/harshil/zach/fitnesstracker/RunningPage.java
@@ -0,0 +1,65 @@
+package com.harshil.zach.fitnesstracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.github.lzyzsd.circleprogress.DonutProgress;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class RunningPage extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference mDatabase;
+    FirebaseUser user;
+    int userExp;
+    DonutProgress progress;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_running_page);
+
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+
+        mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+                User user = dataSnapshot.child("Users").child(currentUser.getUid()).getValue(User.class);
+                userExp = user.xp();
+
+                int toNextRank = userExp%100;
+                progress = findViewById(R.id.donut_progress);
+                progress.setDonut_progress(Integer.toString(toNextRank));
+
+            }
+
+
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                //Something bad happened
+            }
+        });
+
+
+
+
+
+
+    }
+}
