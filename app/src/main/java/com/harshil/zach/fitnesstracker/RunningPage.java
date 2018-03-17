20a8e397b43f6b1349cb1574f7f1a5399d33c79a
Unknown <harshil57@yahoo.com>
2018-03-17 15:57:30 -0400

    Added running challenge page
    The challenge description page UI is done. The amp is added. This just needs to be connected to firebase to pull the data

--- app/src/main/java/com/harshil/zach/fitnesstracker/RunningPage.java
+++ app/src/main/java/com/harshil/zach/fitnesstracker/RunningPage.java
@@ -1,7 +1,10 @@
 package com.harshil.zach.fitnesstracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.github.lzyzsd.circleprogress.DonutProgress;
@@ -48,17 +51,11 @@ public class RunningPage extends AppCompatActivity {
 
             }
 
-
-
             @Override
             public void onCancelled(DatabaseError databaseError) {
                 //Something bad happened
             }
         });
-
-
-
-
 
 
     }
