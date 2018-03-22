6c996b39abc8e00b75a6a703249be179458199f1
Unknown <harshil57@yahoo.com>
2018-03-22 01:10:14 -0400

    Running page updated to show all challenges

--- app/src/main/java/com/harshil/zach/fitnesstracker/RunningChallengePage.java
+++ app/src/main/java/com/harshil/zach/fitnesstracker/RunningChallengePage.java
@@ -1,20 +1,14 @@
 package com.harshil.zach.fitnesstracker;
 
-import android.app.ActivityManager;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -24,19 +18,30 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import android.Manifest;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.Iterator;
 
 
-public class RunningChallenge extends AppCompatActivity implements OnMapReadyCallback, ActivityCompat.OnRequestPermissionsResultCallback {
+public class RunningChallengePage extends AppCompatActivity implements OnMapReadyCallback, ActivityCompat.OnRequestPermissionsResultCallback {
 
 
     private GoogleMap mMap;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
     private boolean mPermissionDenied = false;
+    private Integer challengePos;
+    private DatabaseReference mDatabase;
 
 
 
@@ -54,10 +59,20 @@ public class RunningChallenge extends AppCompatActivity implements OnMapReadyCal
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent startChallenge = new Intent(RunningChallenge.this, RunningPage.class);
+                Intent startChallenge = new Intent(RunningChallengePage.this, RunningPage.class);
                 startActivity(startChallenge);
             }
         });
+
+        Intent intent = getIntent();
+        challengePos = intent.getIntExtra("challengeId", 0);
+
+        final TextView description = (TextView) findViewById(R.id.description);
+        description.setText(Integer.toString(challengePos));
+
+
+
+
 
 
     }
